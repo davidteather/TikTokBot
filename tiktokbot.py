3e8456d6b85b8e549a018096f90e0d439008d931
davidteather <davidteather@users.noreply.github.com>
2019-05-25 11:21:12 -0500

    Downloads Most Popular

--- tiktokbot.py
+++ tiktokbot.py
@@ -1,12 +1,46 @@
+# Json Response https://m.tiktok.com/share/item/list?id=&type=5&count=48&minCursor=0&maxCursor=0&_signature=Ajk97hAbXtYLdUf7zgjlDAI5Pf
+# obj -> body -> itemListData -> itemListData[i] -> itemInfos -> video -> urls[0] gives downloadable url
+import requests
+import random
+import string
+import urllib.request
+import glob, os
+import mimetypes
 
-# Notes about the layout of the https://www.tiktok.com/en/trending page
-# video_feed_item -> _ratio_ -> div -> _ratio_wrapper -> _image_card_ attribute background-image url has the
-# same bkg url as modal
 
-# when viceo is clicked on
-# div _global_modal_ _global_modal_mask _global_modal_wrapper_content _global_modal_content _video_card_big
-# (_image_card _video_card_big_left _video_card_mute_autoshow)
-# (_video_card_play_btn_ _video_card_play_btn_light _video_card_playbtn_wraaper _video_card_ src= direct link to video
-# lowest link
-# https://v16.muscdn.com/6cb14c4279feb7bf99c8ab817bb78279/5ce8c80c/video/tos/maliva/tos-maliva-v-0068/661c1d7273cf4b3dac725b4b7aafc4e8/
-# Can't find where the 661 part comes from
+def clearTMP(delpath):
+    r = glob.glob(delpath)
+    for i in r:
+        os.remove(i)
+
+def sig_generator(stringLength=26):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+# clearTMP('downloaded/*')
+
+count = 10
+url = "https://m.tiktok.com/share/item/list?id=&type=5&count=" + str(count) +"&minCursor=0&maxCursor=0&_signature=NMGxyxAeaC89jcve1KWrdDTBsd"
+r = requests.get(url, headers={"authority": "m.tiktok.com", "method": "GET", "path": url.split("https://m.tiktok.com")[0], "scheme": "https", "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                               "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.9", "cache-control": "max-age=0", "upgrade-insecure-requests": "1",
+                               "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
+
+
+data = r.json()
+i = count - 1
+
+while (i >= 0):
+    try:
+        r = requests.get(data["body"]["itemListData"][i]["itemInfos"]["video"]["urls"][0], allow_redirects=False)
+        content_type = r.headers['content-type']
+        extension = mimetypes.guess_extension(content_type)
+        open('downloaded/' + str(i) + extension, "wb").write(r.content)
+        open("downloaded/concat.txt", 'a').write("file " + str(i) + ".mkv" + "\n")
+        os.system("ffmpeg -i downloaded/" + str(i) + extension + " -c copy -map 0 downloaded/" + str(i) + ".mkv")
+        i -= 1
+    except:
+        i -= 1
+
+# concat errors
+# ffmpeg -f concat -i downloaded/concat.txt -safe 1 -r 30 -fflags +genpts -c:a copy output.mp4
