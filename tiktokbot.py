6f31677a73fa23b576aefb6d9fd1d38eda8b17b8
davidteather <davidteather@users.noreply.github.com>
2019-08-27 15:29:23 -0500

    Added new signature

--- tiktokbot.py
+++ tiktokbot.py
@@ -1,4 +1,4 @@
-# Json Response https://m.tiktok.com/share/item/list?id=&type=5&count=48&minCursor=0&maxCursor=0&_signature=Ajk97hAbXtYLdUf7zgjlDAI5Pf
+# Json Response https://m.tiktok.com/share/item/list?id=&type=5&count=48&minCursor=0&maxCursor=0&_signature=z9nIxxAWkszGlbLSXGTDqM.ZyN
 # obj -> body -> itemListData -> itemListData[i] -> itemInfos -> video -> urls[0] gives downloadable url
 import requests
 import random
@@ -29,7 +29,7 @@ count = 42
 val = True
 while val is True:
     url = "https://m.tiktok.com/share/item/list?id=&type=5&count=" + \
-        str(count) + "&minCursor=0&maxCursor=0&_signature=" + "5GW1lhAbuJftKc-DE.IL-ORltY"
+        str(count) + "&minCursor=0&maxCursor=0&_signature=" + "W5cHbxAVBvNS2316KIvp21uXB3"
     r = requests.get(url, headers={"authority": "m.tiktok.com", "method": "GET", "path": url.split("https://m.tiktok.com")[0], "scheme": "https", "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
                                    "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.9", "cache-control": "max-age=0", "upgrade-insecure-requests": "1",
                                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
